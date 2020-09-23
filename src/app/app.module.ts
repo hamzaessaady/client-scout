0bc7c15635ec44de7a9c180d888baec42e3db0d1
Kuohamkun <50623506+Kuohamkun@users.noreply.github.com>
2020-09-24 00:59:48 +0100

    [Login] Create the login UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
